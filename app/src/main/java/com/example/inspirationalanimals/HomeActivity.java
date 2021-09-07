2d5466b2e499d003fa35eb132bf79110c3a1be66
GuillermoFloresV <guillermo.fifth@gmail.com>
2021-09-07 10:48:25 -0700

    Added onCreateViewHolder inside of InspirationAdapter

--- app/src/main/java/com/example/inspirationalanimals/HomeActivity.java
+++ app/src/main/java/com/example/inspirationalanimals/HomeActivity.java
@@ -3,11 +3,31 @@ package com.example.inspirationalanimals;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.inspirationalanimals.adapters.InspirationAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeActivity extends AppCompatActivity {
+    List<Quote> quotes;
+    List<Dog> dogs;
+    List<Cat> cats;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        RecyclerView rv = findViewById(R.id.rvAnimal);
+        quotes = new ArrayList<>();
+        cats = new ArrayList<>();
+        dogs = new ArrayList<>();
+
+        InspirationAdapter inspirationAdapter = new InspirationAdapter(this, quotes);
+
+        rv.setAdapter(inspirationAdapter);
+
+        //call the APIs
     }
 }
--- app/src/main/java/com/example/inspirationalanimals/adapters/InspirationAdapter.java
+++ app/src/main/java/com/example/inspirationalanimals/adapters/InspirationAdapter.java
@@ -1,17 +1,35 @@
 package com.example.inspirationalanimals.adapters;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.inspirationalanimals.Quote;
+import com.example.inspirationalanimals.R;
+
+import java.util.List;
+
 public class InspirationAdapter extends RecyclerView.Adapter<InspirationAdapter.ViewHolder> {
+    Context context;
+    public List<Quote> quotes;
+    public ImageView img;
+
+    public InspirationAdapter(Context context, List<Quote> quotes){
+        this.context = context;
+        this.quotes = quotes;
+    }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View quoteView = LayoutInflater.from(context).inflate(R.layout.item_quote, parent, false);
+        return new ViewHolder(quoteView);
     }
 
     @Override
